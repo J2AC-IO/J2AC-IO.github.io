16cbb80fb8f12d7600d051f8b7404d680e1410c0
J2AC_IO <101293959+J2AC-IO@users.noreply.github.com>
2024-10-10 19:43:20 -0300

    upload do site

---
+++ script.js
@@ -0,0 +1,10 @@
+function showSurprise() {
+    const message = document.getElementById("romanticMessage");
+    message.innerText = "Obrigado por ser tudo que eu preciso nos piores e nos melhores momentos! Sei que papai do seu não poderia me dar alguém mais extraordinária do que você! Tu és perfeita, minha vida, meu tudo, minha futura esposa! A únicas certezas da vida que eu tenho são apenas três! É você faz parte de todas elas. Feliz 10 meses Baby! Te amo tanto e ainda nem te amo tudo.";
+
+    // Adiciona um efeito de destaque ao texto
+    message.style.transition = "all 0.5s ease";
+    message.style.color = "#d81b60";
+    message.style.fontWeight = "bold";
+    message.style.fontSize = "1.6em";
+}
